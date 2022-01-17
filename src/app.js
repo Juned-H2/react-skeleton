9d40437a9473ad6e58915ff79fa537e45cbacec7
Juned Hussain <juned.hussa@outlook.com>
2022-01-17 23:42:08 +0000

    add .gitignore, integrate normalize.css

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Index from './components/index';
+import 'normalize.css';
 
 ReactDOM.render(<Index />, document.getElementById('app'))
